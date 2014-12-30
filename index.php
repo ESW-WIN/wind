a0270985b30689280f0394a623c9a202f0868a76
GregChan <gregorywarrenchan@gmail.com>
2014-12-29 22:44:22 -0500

    graph modifications

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 ob_start();
 ?>
 <div class="row">
-	<div class="col-md-offset-2 col-md-8">
-		<div data-my-chart>
+	<div class="col-md-offset-1 col-md-10">
+		<div class="row">
+			<div data-wind-speed-chart class="col-md-12"></div>
+		</div>
+		<div class="row">
+			<div data-pressure-chart class="col-md-6"></div>
+			<div data-temperature-chart class="col-md-6"></div>
+		</div>
+		<div>
+			<h4 data-wind-speed></h4>
+			<h4 data-pressure></h4>
+			<h4 data-temperature></h4>
 		</div>
 	</div>
 </div>
--- js/chart.js
+++ js/chart.js
@@ -13,34 +13,65 @@ $.ajax({
 			pressureValues.push([data[i]['time'] * 1000, data[i]['pressure']]);
 		}
 
-		generateLinePlot(windSpeedValues, 'Wind Speed', 'time', 'speed');
-		generateLinePlot(temperatureValues, 'Temperature', 'time', 'temperature');
-		generateLinePlot(pressureValues, 'Pressure', 'time', 'pressure');
+		generateLinePlot('[data-wind-speed-chart]', windSpeedValues, {
+			title: 'Wind Speed',
+			xlabel: 'time',
+			ylabel: 'speed'
+		});
+		generateLinePlot('[data-pressure-chart]', pressureValues, {
+			title: 'Pressure',
+			xlabel: 'time',
+			ylabel: 'pressure',
+			color: '#000000'
+		});
+		generateLinePlot('[data-temperature-chart]', pressureValues, {
+			title: 'Temperature',
+			xlabel: 'time',
+			ylabel: 'temperature',
+			color: '#EE0000'
+		});
+		showLabelValue('[data-wind-speed]', 'Wind Speed: ', ' mph', windSpeedValues[windSpeedValues.length - 1][1]);
+		showLabelValue('[data-temperature]', 'Temperature: ', ' &#176;F', temperatureValues[temperatureValues.length - 1][1]);
+		showLabelValue('[data-pressure]', 'Pressure: ', ' inHg', pressureValues[pressureValues.length - 1][1]);
 	}
 });
 
-var generateLinePlot = function(values, title, xlabel, ylabel) {
-	$('[data-my-chart]').highcharts({
-		title: {
-			text: title
-		},
-		xAxis: {
-			type: 'datetime',
+var generateLinePlot = function(element_label, values, attributes) {
+		var title = attributes.title || "",
+			xlabel = attributes.xlabel || "",
+			ylabel = attributes.ylabel || "",
+			color = attributes.color || null;
+		$(element_label).highcharts({
 			title: {
-				text: xlabel
-			}
-		},
-		yAxis: {
-			title: {
-				text: ylabel
-			}
-		},
-		legend: {
-			enabled: false
-		},
-		series: [{
-			name: title,
-			data: values
-		}]
-	});
-}
+				text: title
+			},
+			chart: {
+				height: 250
+			},
+			xAxis: {
+				type: 'datetime',
+				title: {
+					text: xlabel
+				}
+			},
+			yAxis: {
+				title: {
+					text: ylabel
+				},
+				floor: 0
+			},
+			legend: {
+				enabled: false
+			},
+			series: [{
+				type: 'spline',
+				name: title,
+				data: values,
+				color: color,
+				lineWidth: 3
+			}]
+		});
+	},
+	showLabelValue = function(element_label, before, after, value) {
+		$(element_label).html(before + value + after);
+	};
